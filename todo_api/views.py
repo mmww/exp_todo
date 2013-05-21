55ba88f757355e5adec2ee919355f1d55fdea84e
“mmww” <watts_marcus@hotmail.com>
2013-05-21 16:56:01 +0200

    Reimplement the views that handle the different REST API calls with plain django views without using a tool like tastipy or piston.

--- todo_api/views.py
+++ todo_api/views.py
@@ -1,10 +1,14 @@
 # encoding: utf-8
 
 import logging
-
+from django.shortcuts import render_to_response, get_object_or_404 
+from django.template import RequestContext 
+from django.http import HttpResponse  
 logger = logging.getLogger(__name__)
 
 
 def handle_rest_call(request, *args, **kwargs):
     logger.debug('TODO: Implement me: args %s, kwargs %s', args, kwargs)
-    raise NotImplementedError('TODO: Implement me!')
+    return render_to_response('todo_client/index.html', {},  
+            context_instance=RequestContext(request))
+    #raise NotImplementedError('TODO: Implement me!')
